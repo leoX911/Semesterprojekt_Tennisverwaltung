7c4ff170f858d6a289cab3d2e41e4e72c99df09a
leoX911 <leoxohne@gmail.com>
2021-05-21 00:13:55 +0200

    bb
    b

---
+++ Tennisverwaltungssystem/Tennisverwaltungssystem/PL/frm_Menü_m/UserControls/UserControl1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Tennisverwaltungssystem.BL;
+
+namespace Tennisverwaltungssystem.PL.frm_Menü_m.UserControls
+{
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+            User user = Übersicht_M._user;
+            textBox1.Text=user.EMail;
+        }
+    }
+}
