eaaca0ae2d03db2dbb7dd8f4428a43f4aeb62406
leoX911 <leoxohne@gmail.com>
2021-05-29 16:51:19 +0200

    h

---
+++ Tennisverwaltungssystem/Tennisverwaltungssystem/BL/MsgBox.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tennisverwaltungssystem.BL
+{
+    class MsgBox
+    {
+    }
+}
